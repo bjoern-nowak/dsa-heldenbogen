7e94a7a250a9e197d64a49096a3f9d6e4ab685de
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2022-12-31 16:36:27 +0100

    DSAHB-000: add ASP/clingo example

---
+++ tests/example.py
@@ -0,0 +1,33 @@
+import unittest
+
+from clingo.control import Control
+from clingo.symbol import Number
+
+
+class Context:
+    def inc(self, x):
+        return Number(x.number + 1)
+
+    def seq(self, x, y):
+        return [x, y]
+
+
+class ClingoExample(unittest.TestCase):
+    def test_asp(self):
+        ctl = Control()
+        ctl.load("example.lp")
+        ctl.ground([("base", [])], context=Context())
+        print(ctl.solve(on_model=self.__on_model))
+
+    def __on_model(self, m):
+        print(m)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
