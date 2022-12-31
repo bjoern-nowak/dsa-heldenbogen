887ac478ce019a08643ce83e89de18458873b4c2
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2022-12-31 16:38:35 +0100

    DSAHB-000: enable web development (add dummy app)

---
+++ dsa_heldenbogen/main.py
@@ -0,0 +1,11 @@
+import uvicorn
+
+config: uvicorn.Config = uvicorn.Config(
+    "app:app",  # <file_path>:<variable>
+    reload=True,  # automatically load changed py-files
+    log_level='debug',  # values: critical, error, warning, info, debug, trace
+)
+
+if __name__ == "__main__":
+    server = uvicorn.Server(config)
+    server.run()
