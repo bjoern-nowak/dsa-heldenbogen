5eb140337404f25845fc4abbd017fa6e2158d0bb
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-01-08 13:21:00 +0100

    DSAHB-000: simplify root src folder name

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,5 +2,5 @@
 
 CURRENT_SCRIPT_DIR="$(dirname $(readlink --canonicalize $0))"
 ROOT_DIR="$CURRENT_SCRIPT_DIR/.."
-SRC_DIR="$ROOT_DIR/dsa_heldenbogen"
+SRC_DIR="$ROOT_DIR/app"
 TESTS_DIR="$ROOT_DIR/tests"
