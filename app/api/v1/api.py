6f880ecb711c0b7387d29d22cf0aae620805841c
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-02-13 08:22:18 +0100

    DSAHB-000: provide nedpoint for swagger api doc in yaml

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,8 +1,33 @@
+import functools
+import io
+
+import yaml
+from fastapi import APIRouter
 from fastapi import FastAPI
+from fastapi.responses import Response
+from starlette.responses import RedirectResponse
 
 from app.api.v1.endpoints import held
-from app.api.v1.endpoints import root
 
 api = FastAPI()
-api.include_router(root.router, tags=["root"])
+
+router = APIRouter()
+
+
+@api.get("/", tags=["root"])
+def index():
+    # TODO we shall not must use the api prefix manually
+    return RedirectResponse(url='/api/v1/docs')
+
+
+# Source: https://github.com/tiangolo/fastapi/issues/1140
+@api.get('/openapi.yaml', include_in_schema=False, tags=["root"])
+@functools.lru_cache()  # TODO: is this required only for async?
+def read_openapi_yaml():
+    openapi_json = api.openapi()
+    yaml_s = io.StringIO()
+    yaml.dump(openapi_json, yaml_s, sort_keys=False, allow_unicode=True)
+    return Response(yaml_s.getvalue(), media_type='text/yaml')
+
+
 api.include_router(held.router, prefix="/held", tags=["held"])
