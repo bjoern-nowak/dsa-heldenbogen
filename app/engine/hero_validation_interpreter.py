907b1563f4af6b5714f25dc984b2c9cd17c25d33
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-05 15:47:06 +0100

    DSAHB-000: enable professions to require talents on a level

--- app/engine/hero_validation_interpreter.py
+++ app/engine/hero_validation_interpreter.py
@@ -1,23 +1,37 @@
+from enum import Enum
+
 from clingo import Symbol
 
 from app.engine.rulebook_function import RulebookFunction
 
 
+class HeroValidationErrors(str, Enum):
+    TALENT = 'talent'
+
+
 class HeroValidationInterpreter:
 
-    # TODO return an error object which also holds a type, usable in frontend and testcases
+    # TODO return an error object which also holds an error type and each reason argument, usable in frontend and testcases
     @staticmethod
     def str(error: Symbol) -> str:
+        reason = error.arguments[0]
         match error.name:
             case RulebookFunction.SPECIES_UNKNOWN:
-                return f"Species '{error.arguments[0].string}' is unknown."
+                return f"Species '{reason.string}' is unknown."
             case RulebookFunction.CULTURE_UNKNOWN:
-                return f"Culture '{error.arguments[0].string}' is unknown."
+                return f"Culture '{reason.string}' is unknown."
             case RulebookFunction.CULTURE_UNUSABLE:
-                return f"Culture is unusable for {error.arguments[0].name}."
+                return f"Culture is unusable for {reason.name}."
             case RulebookFunction.PROFESSION_UNKNOWN:
-                return f"Profession '{error.arguments[0].string}' is unknown."
+                return f"Profession '{reason.string}' is unknown."
             case RulebookFunction.PROFESSION_UNUSABLE:
-                return f"Profession is unusable for {error.arguments[0].name}."
+                return f"Profession is unusable for {reason.name}."
+            case RulebookFunction.PROFESSION_MISSING:
+                match reason.name:
+                    case HeroValidationErrors.TALENT:
+                        return f"Profession is missing talent '{reason.arguments[0].string}' on minimum level {reason.arguments[1]}."
+                    case _:
+                        return f"(Generic; non pre-defined)" \
+                               f"Profession is missing {reason.name} with '{reason.arguments}'."
             case _:
                 return f"Unspecified error '{error.name}' for '{error.arguments}'."
