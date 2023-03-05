31bd36f73f56f7c098f9e91add395f32e068e114
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-05 15:48:03 +0100

    DSAHB-000: implement profession 'Söldner' talent requirements hence user other profession for simple tests

--- tests/service/test_hero_service.py
+++ tests/service/test_hero_service.py
@@ -11,20 +11,20 @@ class TestHeroService(unittest.TestCase):
     service = HeroService()
 
     @parameterized.expand([
-        (0, 'Elf', 'Auelfen', 'Söldner'),
+        (0, 'Elf', 'Auelfen', 'Händler'),
         (0, 'Elf', 'Auelfen', 'Zauberweber'),
-        (1, 'Elf', 'Andergaster', 'Söldner'),
-        (1, 'Elf', 'Ambosszwerge', 'Söldner'),
+        (1, 'Elf', 'Andergaster', 'Händler'),
+        (1, 'Elf', 'Ambosszwerge', 'Händler'),
 
-        (0, 'Mensch', 'Andergaster', 'Söldner'),
-        (1, 'Mensch', 'Auelfen', 'Söldner'),
+        (0, 'Mensch', 'Andergaster', 'Händler'),
+        (1, 'Mensch', 'Auelfen', 'Händler'),
         (1, 'Mensch', 'Andergaster', 'Zauberweber'),
-        (1, 'Mensch', 'Ambosszwerge', 'Söldner'),
+        (1, 'Mensch', 'Ambosszwerge', 'Händler'),
 
-        (0, 'Zwerg', 'Ambosszwerge', 'Söldner'),
-        (1, 'Zwerg', 'Auelfen', 'Söldner'),
+        (0, 'Zwerg', 'Ambosszwerge', 'Händler'),
+        (1, 'Zwerg', 'Auelfen', 'Händler'),
         (1, 'Zwerg', 'Ambosszwerge', 'Zauberweber'),
-        (1, 'Zwerg', 'Andergaster', 'Söldner'),
+        (1, 'Zwerg', 'Andergaster', 'Händler'),
     ])
     def test_validation(self,
                         error_count: int,
