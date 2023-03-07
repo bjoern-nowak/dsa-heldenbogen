d984ee05406c57748242ffd53a8a1e65e07bba38
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-07 15:50:33 +0100

    DSAHB-000: improve SoC

--- app/engine/engine.py
+++ app/engine/engine.py
@@ -1,22 +1,25 @@
+from typing import Any
+from typing import Callable
 from typing import List
+from typing import Optional
+from typing import Sequence
+from typing import Tuple
 
 from clingo import Control
+from clingo import Model
 from clingo import SolveResult
 from clingo import Symbol
 
 from app.engine.collector import Collector
-from app.engine.hero_validation_interpreter import HeroValidationError
 from app.engine.hero_validation_interpreter import HeroValidationInterpreter
 from app.engine.hero_wrapper import HeroWrapper
-from app.engine.rulebook_function import RulebookFunction
 from app.engine.rulebook_program import RulebookProgram
 from app.engine.rulebook_validator import RulebookValidator
+from app.error import UnexpectedResultError
 from app.error import UnusableRulebookError
 from app.models import Feature
 from app.models import Hero
 from app.models import Rulebook
-
-_DEFAULT_HERO_VALIDATION_STEPS = [100, 200, 300]
 
 
 class Engine:
@@ -28,26 +31,29 @@ class Engine:
         300: RulebookProgram.VALIDATE_HERO_STEP_300,
         350: RulebookProgram.VALIDATE_HERO_STEP_350,
     }
+    DEFAULT_HERO_VALIDATION_STEPS = hero_validation_steps.keys()
 
     def __init__(self, rulebooks: List[Rulebook]) -> None:
-        self.ctl = self._create_control(RulebookValidator.filter(rulebooks))
+        self.rulebooks = RulebookValidator.filter(rulebooks)
+        self.ctl = self._create_control()
         self._check_rulebooks_usable()
         self._find_extra_hero_validation_steps()
-        # sort by keys
-        self.hero_validation_steps = dict(sorted(self.hero_validation_steps.items()))
+        self.hero_validation_steps = dict(sorted(self.hero_validation_steps.items()))  # sort by keys
 
-    @staticmethod
-    def _create_control(rulebooks: List[Rulebook]) -> Control:
+    def _create_control(self) -> Control:
         ctl = Control()
         ctl.load(Rulebook.common_file())
-        for rulebook in rulebooks:
+        for rulebook in self.rulebooks:
             ctl.load(rulebook.entrypoint())
         return ctl
 
     def _check_rulebooks_usable(self) -> None:
-        self.ctl.ground([RulebookProgram.RULEBOOK_USABLE])
         unusables: List[List[str]] = []
-        self.ctl.solve(on_model=lambda m: Collector.functions_strings(unusables, m, RulebookFunction.RULEBOOK_UNUSABLE))
+        self._execute(
+            ground_parts=[RulebookProgram.RULEBOOK_USABLE],
+            on_model=lambda m: Collector.unusable_rulebooks(m, unusables),
+            on_fail_raise=UnexpectedResultError(f"Failed to collect unusable rulebooks.")
+        )
         if unusables:
             messages = []
             for a in unusables:
@@ -56,11 +62,15 @@ class Engine:
 
     def _find_extra_hero_validation_steps(self) -> None:
         steps: List[Symbol] = []
-        self.ctl.ground([RulebookProgram.META])
-        self.ctl.solve(on_model=lambda m: Collector.functions(steps, m, [RulebookFunction.EXTRA_HERO_VALIDATION_STEP]))
+        self._execute(
+            ground_parts=[RulebookProgram.META],
+            on_model=lambda m: Collector.extra_hero_validation_steps(m, steps),
+            on_fail_raise=UnexpectedResultError(f"Failed to find extra hero validation steps.")
+        )
+
         for step in [step.arguments[0].number for step in steps]:
             self.hero_validation_steps[step] = step
-            if step in _DEFAULT_HERO_VALIDATION_STEPS:
+            if step in self.DEFAULT_HERO_VALIDATION_STEPS:
                 # TODO make it a warning, and it should be a testcase instead of a runtime check
                 print(f"Some of the given rulebooks redeclare the default hero validation step '{step}' as an additional."
                       f"It does not harm but it is not recommended.")
@@ -68,22 +78,37 @@ class Engine:
     def validate(self, hero: Hero) -> List[str] | None:
         errors = []
         for step in self.hero_validation_steps:
-            errors = self._validate_step(hero, step)
+            program = step if isinstance(step, RulebookProgram) else RulebookProgram.hero_validation_step_for(step)
+            errors = self._perform_hero_validation_step(hero, program)
             if errors:
                 break
         return errors
 
-    def _validate_step(self, hero: Hero, step: RulebookProgram | int) -> List[str] | None:
-        program = step if isinstance(step, RulebookProgram) else RulebookProgram.hero_validation_step_for(step)
-        self.ctl.ground([RulebookProgram.BASE, RulebookProgram.HERO_FACTS, program], HeroWrapper(hero))
+    def _perform_hero_validation_step(self, hero: Hero, program: Tuple[str, Sequence[Symbol]]) -> List[str] | None:
         errors: List[Symbol] = []
-        result: SolveResult = self.ctl.solve(on_model=lambda m: Collector.functions(errors, m, HeroValidationError.list()))
-        if not result.satisfiable:
-            return None
+        self._execute(
+            ground_parts=[RulebookProgram.BASE, RulebookProgram.HERO_FACTS, program],
+            context=HeroWrapper(hero),
+            on_model=lambda m: Collector.hero_validation_errors(m, errors),
+            on_fail_raise=UnexpectedResultError(f"Hero validation could not be performed at: {program[0]}")
+        )
         return [HeroValidationInterpreter.str(e) for e in errors]
 
     def list(self, feature: Feature) -> List[str]:
-        self.ctl.ground()
-        features: List[str] = []
-        self.ctl.solve(on_model=lambda m: Collector.functions_first_string(features, m, RulebookFunction.known(feature)))
-        return features
+        known_values: List[str] = []
+        self._execute(
+            on_model=lambda m: Collector.known_feature_values(m, known_values, feature),
+            on_fail_raise=UnexpectedResultError(f"Value listing for feature '{feature}' failed.")
+        )
+        return known_values
+
+    def _execute(self,
+                 ground_parts: Sequence[Tuple[str, Sequence[Symbol]]] = (RulebookProgram.BASE,),
+                 context: Any = None,
+                 on_model: Optional[Callable[[Model], Optional[bool]]] = None,
+                 on_fail_raise: Exception = None):
+        self.ctl.ground(ground_parts, context)
+        result: SolveResult = self.ctl.solve(on_model=on_model)
+        if not result.satisfiable:
+            if on_fail_raise:
+                raise on_fail_raise
--- app/service/hero_service.py
+++ app/service/hero_service.py
@@ -1,7 +1,7 @@
+import logging
 from typing import List
 
 from app.engine.engine import Engine
-from app.error import UnexpectedResultError
 from app.models import Hero
 from app.models.rulebook import Rulebook
 
@@ -11,6 +11,4 @@ class HeroService:
     def validate(self, hero: Hero, rulebooks: List[Rulebook]) -> List[str]:
         engine = Engine(rulebooks)
         errors: List[str] = engine.validate(hero)
-        if errors is None:
-            raise UnexpectedResultError("Hero has not been validated.")
         return errors
