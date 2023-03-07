3d753b66c3b20bb97be6d623827761f56b1c3af0
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-07 15:42:50 +0100

    DSAHB-000: add documentation to rulebook programs

--- app/engine/rulebook_program.py
+++ app/engine/rulebook_program.py
@@ -9,19 +9,32 @@ _VALIDATE_HERO_STEP = 'validate_hero_step'
 
 class RulebookProgram(tuple[str, Sequence[Symbol]], BaseEnum):
     BASE = ('base', [])
+    """Must contain all known feature values and restrictions"""
+
     RULEBOOK_USABLE = ('rulebook_usable', [])
+    """Checks whenever the rulebook requirements are met"""
+
     META = ('meta', [])
+    """May contain information about the rulebook for processing, like extra hero validation steps"""
+
     HERO_FACTS = ('hero_facts', [])
+    """Must contain all facts about the hero which can be directly taken from it"""
+
     VALIDATE_HERO_STEP_100 = (f"{_VALIDATE_HERO_STEP}_100", [])
     """Validates hero species usable"""
+
     VALIDATE_HERO_STEP_150 = (f"{_VALIDATE_HERO_STEP}_150", [])
     """Validates hero species requirements"""
+
     VALIDATE_HERO_STEP_200 = (f"{_VALIDATE_HERO_STEP}_200", [])
     """Validates hero culture usable"""
+
     VALIDATE_HERO_STEP_250 = (f"{_VALIDATE_HERO_STEP}_250", [])
     """Validates hero culture requirements"""
+
     VALIDATE_HERO_STEP_300 = (f"{_VALIDATE_HERO_STEP}_300", [])
     """Validates hero profession usable"""
+
     VALIDATE_HERO_STEP_350 = (f"{_VALIDATE_HERO_STEP}_350", [])
     """Validates hero profession requirements"""
 
