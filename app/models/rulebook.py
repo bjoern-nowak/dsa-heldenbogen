ff4b8a811b9ddbee0f21f91700a74f148f5222f1
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-07 15:42:09 +0100

    DSAHB-000: add extension example to available rulebooks

--- app/models/rulebook.py
+++ app/models/rulebook.py
@@ -12,6 +12,7 @@ class Rulebook(str, BaseEnum):
     DSA5 = 'dsa5'
     DSA5_AVENTURISCHES_KOMPENDIUM_2 = 'dsa5_aventurisches_kompendium_2'
     DSA5_AVENTURISCHES_GOETTERWIRKEN_2 = 'dsa5_aventurisches_götterwirken_2'
+    DSA5_EXTENSION_EXAMPLE = 'dsa5_extension_example'
 
     def entrypoint(self) -> str:
         return get_abs_path(f"{_RULEBOOK_RES_FOLDER}/{self.value}/{_RULEBOOK_ENTRYPOINT}")
