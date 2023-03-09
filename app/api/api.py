686263589a932584c0f04f49e4d89ae265e84419
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-09 21:31:11 +0100

    DSAHB-000: keep api (incl. documentation) clean and tight by having only one

--- app/api/api.py
+++ app/api/api.py
@@ -1,35 +1,9 @@
-import functools
-import io
-
-import yaml
 from fastapi import APIRouter
-from fastapi import FastAPI
-from fastapi.responses import Response
-from starlette.responses import RedirectResponse
 
 from app.api.endpoints import hero
 from app.api.endpoints import meta
 
-api = FastAPI()
-
 router = APIRouter()
 
-
-@api.get('/', tags=['root'])
-def index():
-    # TODO we shall not must use the api prefix manually
-    return RedirectResponse(url='/api/docs')
-
-
-# Source: https://github.com/tiangolo/fastapi/issues/1140
-@api.get('/openapi.yaml', include_in_schema=False, tags=['root'])
-@functools.lru_cache()  # TODO: is this required only for async?
-def read_openapi_yaml():
-    openapi_json = api.openapi()
-    yaml_s = io.StringIO()
-    yaml.dump(openapi_json, yaml_s, sort_keys=False, allow_unicode=True)
-    return Response(yaml_s.getvalue(), media_type='text/yaml')
-
-
-api.include_router(hero.router, prefix='/hero', tags=['hero'])
-api.include_router(meta.router, prefix='/meta', tags=['meta'])
+router.include_router(meta.router, prefix='/meta', tags=['meta'])
+router.include_router(hero.router, prefix='/hero', tags=['hero'])
--- app/api/root.py
+++ app/api/root.py
@@ -1,22 +1,30 @@
-from typing import List
+import functools
+import io
 
+import yaml
 from fastapi import FastAPI
+from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 
-from .api import api
+from . import api
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', tags=['root'])
 def index():
     return RedirectResponse(url='/docs')
 
 
-@app.get('/apis')
-def index() -> List[str]:
-    """List all available apis"""
-    return ['/api']
+# Author: https://github.com/hjoukl
+# Source: https://github.com/tiangolo/fastapi/issues/1140
+@app.get('/openapi.yaml', include_in_schema=True, tags=['root'])
+@functools.lru_cache()  # TODO: is this required only for async?
+def read_openapi_yaml():
+    openapi_json = app.openapi()
+    yaml_s = io.StringIO()
+    yaml.dump(openapi_json, yaml_s, sort_keys=False, allow_unicode=True)
+    return Response(yaml_s.getvalue(), media_type='text/yaml')
 
 
-app.mount('/api', api)
+app.include_router(api.router, prefix='/api')
