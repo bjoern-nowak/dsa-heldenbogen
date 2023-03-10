0490ffb198c0d240d8a16f7f6953358de7e0cc80
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-10 13:14:24 +0100

    DSAHB-000: fix weird fastapi exception on using a schema having inner classes made available for typing trough 'from __future__ import annotations'

--- app/models/hero_validation_error.py
+++ app/models/hero_validation_error.py
@@ -1,5 +1,3 @@
-from __future__ import annotations  # required till PEP 563
-
 from typing import Optional
 
 from app.models.base_enum import BaseEnum
@@ -13,6 +11,15 @@ class HeroValidationError(BaseModel):
     Field 'message' uses single quote for used 'parameters'
     Field 'parameters' contains relevant evaluable data. May not all are used in 'message'.
     """
+
+    class Type(str, BaseEnum):
+        UNKNOWN = 'unknown'
+        UNUSABLE_BY = 'unusable_by'
+        MISSING_LEVEL = 'missing_level'
+
+    class Addon(str, BaseEnum):
+        ANY_OF = 'any_of'
+
     type: Type
     addon: Optional[Addon]
     message: str
@@ -23,11 +30,3 @@ class HeroValidationError(BaseModel):
 
     def __repr__(self) -> str:
         return self.message
-
-    class Type(str, BaseEnum):
-        UNKNOWN = 'unknown'
-        UNUSABLE_BY = 'unusable_by'
-        MISSING_LEVEL = 'missing_level'
-
-    class Addon(str, BaseEnum):
-        ANY_OF = 'any_of'
--- app/models/hero_validation_warning.py
+++ app/models/hero_validation_warning.py
@@ -1,5 +1,3 @@
-from __future__ import annotations  # required till PEP 563
-
 from app.models.base_enum import BaseEnum
 from app.models.base_model import BaseModel
 
@@ -11,6 +9,12 @@ class HeroValidationWarning(BaseModel):
     Field 'message' uses single quote for used 'parameters'
     Field 'parameters' contains relevant evaluable data. May not all are used in 'message'.
     """
+
+    class Type(str, BaseEnum):
+        MISSING_USUAL = 'missing_usual'
+        MISSING_TYPICAL = 'missing_typical'
+        ATYPICAL = 'atypical'
+
     type: Type
     message: str
     parameter: dict[str, str]
@@ -20,8 +24,3 @@ class HeroValidationWarning(BaseModel):
 
     def __repr__(self) -> str:
         return self.message
-
-    class Type(str, BaseEnum):
-        MISSING_USUAL = 'missing_usual'
-        MISSING_TYPICAL = 'missing_typical'
-        ATYPICAL = 'atypical'
