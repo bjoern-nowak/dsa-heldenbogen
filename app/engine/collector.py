edc959559a770e61419d37468b971ca0e79158c1
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-10 18:33:36 +0100

    DSAHB-000: avoid mutating parameters where possible (fix bad practice)

--- app/engine/collector.py
+++ app/engine/collector.py
@@ -15,72 +15,60 @@ class Collector:
 
     @classmethod
     def unusable_rulebooks(cls, model: Model, unusables: List[List[str]]):
-        cls._functions_strings(unusables, model, RulebookFunction.RULEBOOK_UNUSABLE)
+        unusables.extend(cls._functions_strings(model, RulebookFunction.RULEBOOK_UNUSABLE))
 
     @classmethod
     def extra_hero_validation_steps(cls, model: Model, steps: List[Symbol]):
-        cls._functions(steps, model, [RulebookFunction.EXTRA_HERO_VALIDATION_STEP])
+        steps.extend(cls._functions(model, [RulebookFunction.EXTRA_HERO_VALIDATION_STEP]))
 
     @classmethod
     def hero_validation_errors_and_warnings(cls, model: Model, errors: List[Symbol], warnings: List[Symbol]):
-        cls._functions(errors, model, hero_validation_interpreter.ErrorAtom.list())
-        cls._functions(warnings, model, hero_validation_interpreter.WarningAtom.list())
+        errors.extend(cls._functions(model, hero_validation_interpreter.ErrorAtom.list()))
+        warnings.extend(cls._functions(model, hero_validation_interpreter.WarningAtom.list()))
         if errors:
             logger.trace(f"Model of failed hero validation:\n{model.symbols(shown=True)}")
 
     @classmethod
     def known_feature_values(cls, model: Model, known_values: List[str], feature: Feature):
-        cls._functions_first_string(known_values, model, RulebookFunction.known(feature))
-
-    # TODO improve below internal methods NOT filling an list parameter but RETURNING such
+        known_values.extend(cls._functions_first_string(model, RulebookFunction.known(feature)))
 
     @classmethod
-    def _functions_first_string(cls, found: List[str], model: Model, by_name: str = None) -> None:
+    def _functions_first_string(cls, model: Model, by_name: str = None) -> List[str]:
         """
         Collects all first arguments as a string of all functions
-        :param found: list to be filled
-        :param model: to search in
-        :param by_name: (optional) filter functions by given name
-        :return:
         """
-        functions: List[Symbol] = []
-        cls._functions(functions, model)
-        for func in functions:
+        found = []
+        for func in cls._functions(model):
             if not by_name or (by_name == func.name):
                 found.append(func.arguments[0].string)
+        return found
 
     @classmethod
-    def _functions_strings(cls, found: List[List[str]], model: Model, by_name: RulebookFunction = None) -> None:
+    def _functions_strings(cls, model: Model, by_name: RulebookFunction = None) -> List[List[str]]:
         """
         Collects all arguments as a string of all functions
-        :param found: list to be filled
-        :param model: to search in
-        :param by_name: (optional) filter functions by given name
-        :return:
         """
-        functions: List[Symbol] = []
-        cls._functions(functions, model)
-        for func in functions:
+        found = []
+        for func in cls._functions(model):
             if not by_name or (by_name == func.name):
                 found.append([arg.string for arg in func.arguments])
+        return found
 
     @staticmethod
-    def _functions(found: List[Symbol], model: Model, name_whitelist: List[str] = None) -> None:
+    def _functions(model: Model, name_whitelist: List[str] = None) -> List[Symbol]:
         """
-        Collects all functions having given suffix in name
-        :param found: list to be filled
-        :param model: to search in
-        :param name_whitelist: to filter function names by
+        Collects all functions, may filtering by given name whitelist
         :return:
         """
+        found = []
         for sym in model.symbols(atoms=True):
             if not sym.name:  # use 'or not sym.arguments:' to skip constants
                 # skip tuples but keep constants
                 continue
             if not name_whitelist or sym.name in name_whitelist:
                 found.append(sym)
+        return found
 
     @staticmethod
-    def _symbols(found: List[Symbol], model: Model) -> None:
-        for sym in model.symbols(atoms=True):
-            found.append(sym)
+    def _symbols(model: Model) -> List[Symbol]:
+        return [sym for sym in model.symbols(atoms=True)]
