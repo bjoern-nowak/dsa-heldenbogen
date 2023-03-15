19bc30db1d61ff6b679f426773653daf31c8f8e6
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-03-15 15:17:11 +0100

    DSAHB-000: add test cases for all types of common rules + allow other errors to be present + adapt test method accept rulebooks to use + make test method more explicit about fail reason

--- tests/invalid_heros.py
+++ tests/invalid_heros.py
@@ -137,3 +137,183 @@ UNKNOWN_DISADVANTAGE = (
         disadvantages=[DisAdvantage(name='__unknown__')],
     )
 )
+
+CULTURE_UNUSABLE_BY_SPECIES = (
+    HeroValidationError.Type.UNUSABLE_BY,
+    {
+        HeroValidationParam.C_F: 'culture',
+        HeroValidationParam.C_F_VALUE: 'Andergaster',
+        HeroValidationParam.R_F: 'species',
+        HeroValidationParam.R_F_VALUE: 'Zwerg',
+    },
+    Hero(
+        name='valid_söldner',
+        experience_level=ExperienceLevel.AVERAGE,
+        species='Zwerg',
+        culture='Andergaster',
+        profession='Söldner',
+        talents=[],
+        combat_techniques=[],
+        advantages=[],
+        disadvantages=[],
+    )
+)
+
+PROFESSION_UNUSABLE_BY_SPECIES = (
+    HeroValidationError.Type.UNUSABLE_BY,
+    {
+        HeroValidationParam.C_F: 'profession',
+        HeroValidationParam.C_F_VALUE: 'Zauberweber',
+        HeroValidationParam.R_F: 'species',
+        HeroValidationParam.R_F_VALUE: 'Zwerg',
+    },
+    Hero(
+        name='valid_söldner',
+        experience_level=ExperienceLevel.AVERAGE,
+        species='Zwerg',
+        culture='Ambosszwerge',
+        profession='Zauberweber',
+        talents=[],
+        combat_techniques=[],
+        advantages=[],
+        disadvantages=[],
+    )
+)
+
+PROFESSION_UNUSABLE_BY_CULTURE = (
+    HeroValidationError.Type.UNUSABLE_BY,
+    {
+        HeroValidationParam.C_F: 'profession',
+        HeroValidationParam.C_F_VALUE: 'Skuldrun',
+        HeroValidationParam.R_F: 'culture',
+        HeroValidationParam.R_F_VALUE: 'Ambosszwerge',
+    },
+    Hero(
+        name='valid_söldner',
+        experience_level=ExperienceLevel.AVERAGE,
+        species='Zwerg',
+        culture='Ambosszwerge',
+        profession='Skuldrun',
+        talents=[],
+        combat_techniques=[],
+        advantages=[],
+        disadvantages=[],
+    ),
+    ['dsa5', 'dsa5_aventurisches_götterwirken_2']
+)
+
+PROFESSION_MISSING_LEVEL_FOR_TALENT = (
+    HeroValidationError.Type.MISSING_LEVEL,
+    {
+        HeroValidationParam.C_F: 'profession',
+        HeroValidationParam.C_F_VALUE: 'Söldner',
+        HeroValidationParam.R_F: 'talent',
+        HeroValidationParam.R_F_VALUE: 'Körperbeherrschung',
+        HeroValidationParam.MIN_LEVEL: 3,
+    },
+    Hero(
+        name='valid_söldner',
+        experience_level=ExperienceLevel.AVERAGE,
+        species='Zwerg',
+        culture='Ambosszwerge',
+        profession='Söldner',
+        talents=[],
+        combat_techniques=[],
+        advantages=[],
+        disadvantages=[],
+    )
+)
+
+PROFESSION_MISSING_LEVEL_FOR_COMBAT_TECHNIQUE = (
+    HeroValidationError.Type.MISSING_LEVEL,
+    {
+        HeroValidationParam.C_F: 'profession',
+        HeroValidationParam.C_F_VALUE: 'Söldner',
+        HeroValidationParam.R_F: 'combat_technique',
+        HeroValidationParam.R_F_VALUE: 'Armbrüste',
+        HeroValidationParam.MIN_LEVEL: 10,
+    },
+    Hero(
+        name='valid_söldner',
+        experience_level=ExperienceLevel.AVERAGE,
+        species='Zwerg',
+        culture='Ambosszwerge',
+        profession='Söldner',
+        talents=[],
+        combat_techniques=[],
+        advantages=[],
+        disadvantages=[],
+    )
+)
+
+PROFESSION_MISSING_LEVEL_FOR_ANY_OF_COMBAT_TECHNIQUES = (
+    HeroValidationError.Type.MISSING_LEVEL,
+    {
+        HeroValidationParam.C_F: 'profession',
+        HeroValidationParam.C_F_VALUE: 'Söldner',
+        HeroValidationParam.R_F: 'combat_technique',
+        HeroValidationParam.SELECTION: ['Hiebwaffen', 'Schwerter', 'Stangenwaffen', 'Zweihandschwerter',
+                                        'Zweihandhiebwaffen'],
+        HeroValidationParam.MIN_LEVEL: 10,
+        HeroValidationParam.SELECTION_MIN_CHOICES: 1,
+    },
+    Hero(
+        name='valid_söldner',
+        experience_level=ExperienceLevel.AVERAGE,
+        species='Zwerg',
+        culture='Ambosszwerge',
+        profession='Söldner',
+        talents=[],
+        combat_techniques=[],
+        advantages=[],
+        disadvantages=[],
+    )
+)
+
+TALENT_EXCEEDS_MAX_LEVEL_BY_EXPERIENCE = (
+    HeroValidationError.Type.MAX_LVL_EXCEEDED,
+    {
+        HeroValidationParam.C_F: 'talent',
+        HeroValidationParam.C_F_VALUE: 'Körperbeherrschung',
+        HeroValidationParam.C_F_LEVEL: 17,
+        HeroValidationParam.MAX_LEVEL: 16,
+    },
+    Hero(
+        name='valid_söldner',
+        experience_level=ExperienceLevel.MASTERFUL,
+        species='Zwerg',
+        culture='Ambosszwerge',
+        profession='Söldner',
+        talents=Skill.list_by([('Körperbeherrschung', 17), ('Kraftakt', 3), ('Selbstbeherrschung', 4), ('Zechen', 5),
+                               ('Menschenkenntnis', 3), ('Überreden', 3), ('Orientierung', 4), ('Wildnisleben', 3),
+                               ('Götter & Kulte', 3), ('Kriegskunst', 6), ('Sagen & Legenden', 5), ('Handel', 3),
+                               ('Heilkunde Wunden', 4)]),
+        combat_techniques=Skill.list_by([('Armbrüste', 10), ('Raufen', 10), ('Hiebwaffen', 10)]),
+        advantages=[],
+        disadvantages=[],
+    )
+)
+
+COMBAT_TECHNIQUE_EXCEEDS_MAX_LEVEL_BY_EXPERIENCE = (
+    HeroValidationError.Type.MAX_LVL_EXCEEDED,
+    {
+        HeroValidationParam.C_F: 'combat_technique',
+        HeroValidationParam.C_F_VALUE: 'Armbrüste',
+        HeroValidationParam.C_F_LEVEL: 17,
+        HeroValidationParam.MAX_LEVEL: 16,
+    },
+    Hero(
+        name='valid_söldner',
+        experience_level=ExperienceLevel.MASTERFUL,
+        species='Zwerg',
+        culture='Ambosszwerge',
+        profession='Söldner',
+        talents=Skill.list_by([('Körperbeherrschung', 3), ('Kraftakt', 3), ('Selbstbeherrschung', 4), ('Zechen', 5),
+                               ('Menschenkenntnis', 3), ('Überreden', 3), ('Orientierung', 4), ('Wildnisleben', 3),
+                               ('Götter & Kulte', 3), ('Kriegskunst', 6), ('Sagen & Legenden', 5), ('Handel', 3),
+                               ('Heilkunde Wunden', 4)]),
+        combat_techniques=Skill.list_by([('Armbrüste', 17), ('Raufen', 10), ('Hiebwaffen', 10)]),
+        advantages=[],
+        disadvantages=[],
+    )
+)
--- tests/service/test_hero_service.py
+++ tests/service/test_hero_service.py
@@ -1,3 +1,6 @@
+from typing import List
+from typing import Optional
+
 from parameterized import parameterized
 
 from app.engine.exceptions import HeroInvalidError
@@ -19,39 +22,50 @@ class TestHeroService(BaseTestCase):
     service = HeroService()
 
     @parameterized.expand([
-        (invalid_heros.UNKNOWN_SPECIES,),
-        (invalid_heros.UNKNOWN_CULTURE,),
+        (invalid_heros.UNKNOWN_SPECIES,),  # case 0
+        (invalid_heros.UNKNOWN_CULTURE,),  # case 1 ...
         (invalid_heros.UNKNOWN_PROFESSION,),
         (invalid_heros.UNKNOWN_TALENT,),
         (invalid_heros.UNKNOWN_COMBAT_TECHNIQUE,),
         (invalid_heros.UNKNOWN_ADVANTAGE,),
         (invalid_heros.UNKNOWN_DISADVANTAGE,),
+        (invalid_heros.CULTURE_UNUSABLE_BY_SPECIES,),
+        (invalid_heros.PROFESSION_UNUSABLE_BY_SPECIES,),
+        (invalid_heros.PROFESSION_UNUSABLE_BY_CULTURE,),
+        (invalid_heros.PROFESSION_MISSING_LEVEL_FOR_TALENT,),
+        (invalid_heros.PROFESSION_MISSING_LEVEL_FOR_COMBAT_TECHNIQUE,),
+        (invalid_heros.PROFESSION_MISSING_LEVEL_FOR_ANY_OF_COMBAT_TECHNIQUES,),
+        (invalid_heros.TALENT_EXCEEDS_MAX_LEVEL_BY_EXPERIENCE,),
+        (invalid_heros.COMBAT_TECHNIQUE_EXCEEDS_MAX_LEVEL_BY_EXPERIENCE,),
     ])
-    def test_invalid_heros(self, data: tuple[HeroValidationError.Type, dict[HeroValidationParam, str], Hero]):
+    def test_invalid_heros(self,
+                           data: tuple[HeroValidationError.Type, dict[HeroValidationParam, str, Optional[List[str]]], Hero]):
         # given:
-        rulebooks = Rulebook.map(['dsa5'])
         expected_error_type = data[0]
         expected_error_params = data[1]
         invalid_hero = data[2]
+        rulebooks = data[3] if len(data) == 4 else ['dsa5']
         # expect:
         with self.assertRaises(HeroInvalidError) as ctx:
             # when:
-            self.service.validate(invalid_hero, rulebooks)
+            self.service.validate(invalid_hero, Rulebook.map(rulebooks))
         # then:
-        found = False
+        found_type = False
+        found_params = False
         for error in ctx.exception.errors:
             if error.type == expected_error_type:
-                self.assertTrue(
-                    _is_subset_of(expected_error_params, error.parameter),
-                    msg=f"Found error type but does not have expected params."
-                        f"\nexpected params: {expected_error_params}"
-                        f"\nerror params: {error.parameter}"
-                )
-                found = True
-        self.assertTrue(found, msg=f"Did not find error of expected type."
-                                   f"\nexpected type: {expected_error_type}"
-                                   f"\nfound errors: {ctx.exception.errors}"
-                        )
+                found_type = True
+                if _is_subset_of(expected_error_params, error.parameter):
+                    found_params = True
+                    break
+        self.assertTrue(found_type, msg=f"Did not find error of expected type."
+                                        f"\nexpected type: {expected_error_type}"
+                                        f"\nfound errors: {ctx.exception.errors}")
+        params_of_correct_error_type = [str(e.parameter) for e in ctx.exception.errors if e.type == expected_error_type]
+        self.assertTrue(found_params, msg=f"Found error type but does not have expected params."
+                                          f"\nexpected params: {expected_error_params}"
+                                          f"\nfound params:"
+                                          f"\n{chr(10).join(params_of_correct_error_type)}")
 
     @parameterized.expand([
         (valid_heros.SOELDNER,),
