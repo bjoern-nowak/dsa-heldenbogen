0ffa5fa24d4d689d466e0dffa28ce7195cee27ab
bjoern-nowak <dsa-heldenbogen@nowakhub.de>
2023-04-19 15:32:45 +0200

    DSAHB-000: decouple meta service unit test of actual rulebook implementations (using previous introduced support)

--- tests/app/service/test_meta_service.py
+++ tests/app/service/test_meta_service.py
@@ -1,27 +1,29 @@
 from parameterized import parameterized
 
 from dsaheldenbogen.app.models.feature import Feature
-from dsaheldenbogen.app.models.rulebook import Rulebook
 from dsaheldenbogen.app.services.meta_service import MetaService
+from tests.app.engine.engine import TestEngine
+from tests.app.models.rulebook import TestRulebook
 from tests.base_test_case import BaseTestCase
 
 
 class TestMetaService(BaseTestCase):
-    service = MetaService()
+    service = MetaService(TestEngine)
 
     @parameterized.expand([
-        (4, Feature.RACE),
-        (33, Feature.CULTURE),
-        (4, Feature.PROFESSION),
-        (34, Feature.ADVANTAGE),
-        (16, Feature.DISADVANTAGE),
-        (31, Feature.TALENT),
-        (7, Feature.COMBAT_TECHNIQUE),
+        (1, Feature.EXPERIENCE_LEVEL),
+        (2, Feature.RACE),
+        (3, Feature.CULTURE),
+        (1, Feature.PROFESSION),
+        (4, Feature.ADVANTAGE),
+        (3, Feature.DISADVANTAGE),
+        (2, Feature.TALENT),
+        (1, Feature.COMBAT_TECHNIQUE),
     ])
     def test_feature_listing(self, expected_count: int, feature: Feature):
         # given:
-        rulebooks = ['dsa5']
+        rulebooks = ['meta_service']
         # when:
-        found = self.service.list_known_feature_values(feature, Rulebook.map(rulebooks))
+        found = self.service.list_known_feature_values(feature, TestRulebook.map(rulebooks))
         # then:
         self.assertIs(len(found), expected_count)
